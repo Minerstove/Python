c2ff20673afd7a1054001ae89139795ee5bc1d4c
Minerstove <rfgviterbo@gmail.com>
2025-09-03 12:25:20 +0800

    fixed prac 2

--- Practice2.py
+++ Practice2.py
@@ -1,9 +1,5 @@
 #Prac 2a - First Letters
 def first_letters(words):
-    first_letters = ()
-    i=0
-    for i in range(len(words)):
-        first_letters = first_letters + (words[i][0],)
-    return first_letters
+    return tuple((w[0] for w in words),)
     
-print(first_letters(('can', 'i', 'have', 'this', 'dance')))
+#print(first_letters(('can', 'i', 'have', 'this', 'dance')))
