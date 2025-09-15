af541c4dcdeed3aef73e64458bc7b8a2df5bc53b
Minerstove <rfgviterbo@gmail.com>
2025-09-15 19:40:41 +0800

    Fuck practice 4

--- Practice4.py
+++ Practice4.py
@@ -1,11 +1,13 @@
 #Prac 4a - Shaman II: Shampoo
 def jeepney_groups(passengers, jeepney_counts):
-    if len(passengers) == 0:
-        return ()
-    if not jeepney_counts:
-        return ()
     return tuple(passengers[sum(jeepney_counts[:i]) : sum(jeepney_counts[:i+1])] for i in range(len(jeepney_counts)))
 
 print(jeepney_groups(('eiko', 'dagger', 'freya', 'amarant', 'vivi', 'steiner', 'zidane', 'quina'), (3, 4, 1)))
 print(jeepney_groups((), (3, 4, 1)))
 print(jeepney_groups(('eiko', 'dagger', 'freya', 'amarant', 'vivi', 'steiner', 'zidane', 'quina'), ()))
+
+#Prac 4b - Shaman II: Shampoo
+
+#Prac 4c - Banana Queue
+def banana_sequence(flavors):
+    ...
