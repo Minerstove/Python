9d2c6e7a6d57ff87e621ae4df8c0ecd7d7f8b594
Minerstove <rfgviterbo@gmail.com>
2025-09-25 01:54:56 +0800

    Added a playground to learn how to do basic functions recursively/comprehensionally

--- hope-1/(download_and_submit_code_here!)_offline_judge/4_we_know/student_solution.py
+++ hope-1/(download_and_submit_code_here!)_offline_judge/4_we_know/student_solution.py
@@ -2,8 +2,41 @@
 #i can't check constraints, so just be honest hehe
 
 #---to test your code, RUN this file---
-def we_know(k,s):
-    ...
+def we_know(key, s):
+    alphabet = " ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    key = "".join(ch for ch in key if ch.isalpha())  # strip non-letters
+    key = key.upper()
+
+    def shift_val(ch):
+        return alphabet.index(ch)  # A=0, B=1, ...
+
+    def helper(t, j):
+        if not t:
+            return ""
+        ch = t[0]
+        if ch.isalpha():
+            k = shift_val(key[j % len(key)])
+            # shift backwards for decryption
+            p = alphabet[(alphabet.index(ch.upper()) - k) % 26]
+            # preserve case
+            if ch.islower():
+                p = p.lower()
+            return p + helper(t[1:], j + 1)
+        else:
+            return ch + helper(t[1:], j)
+
+    return helper(s, 0)
+
+
+print(we_know("RIGHT", "OAVVA"))
+"""
+def _enumerate(s,i):
+    if len(s) == 0:
+        return ()
+    return ((i, s[0]),) + _enumerate(s[1:], i)
+"""
+
+
 
 #---to test your code, RUN this file---
 
---
+++ playground.py
@@ -0,0 +1,45 @@
+def _enumerate(s,i):
+    if len(s) == 0:
+        return ()
+    return ((i, s[0]),) + _enumerate(s[1:], i + 1)
+#print(_enumerate((213, 2143, 213), 1))
+
+def _max(nums):
+    if len(nums) == 1:
+        return nums[0]
+    
+    if nums[0] >= nums[1]:
+        return _max((nums[0],) + nums[2:])
+    else:
+        return _max(nums[1:])
+#print(_max((213, 2143234, 213213)))
+
+def _min(nums):
+    if len(nums) == 1:
+        return nums[0]
+    
+    if nums[0] <= nums[1]:
+        return _min((nums[0],) + nums[2:])
+    else:
+        return _min(nums[1:])
+#print(_min((213, 2121312343, 213213)))
+
+def _sum(nums):
+    if len(nums) == 0:
+        return 0
+    
+    return nums[0] + _sum(nums[1:])
+#print(_sum((213, 2121312343, 213213)))
+
+def _sorted(nums):
+    def helper():
+        ...
+    
+    return helper()
+print(_sorted((213, 2121312343, 213213)))
+
+def _flipgrid(grid):
+    ...
+
+def _range(num1, num2):
+    ...
