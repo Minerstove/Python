d7a10a561c2513877e7bef51a45dca80259d25da
Minerstove <rfgviterbo@gmail.com>
2025-09-24 22:31:46 +0800

    Added solutions to sample hope1 1 and 2

--- hope-1/(download_and_submit_code_here!)_offline_judge/3_mkns/student_solution.py
+++ hope-1/(download_and_submit_code_here!)_offline_judge/3_mkns/student_solution.py
@@ -3,7 +3,28 @@
 
 #---to test your code, RUN this file---
 def mkns(n,k):
-    ...
+    radius = n**k
+    base = radius**2
+    
+    def get_next_layer(base, radius, height):
+        if base <= 0:
+            return 0
+        new_layer = base - (2*height*radius + radius**2)
+        return new_layer + get_next_layer(new_layer, radius - 1, height + 1)
+    
+    return base + get_next_layer(base, radius, 1)
+
+"""
+    def get_cylinder_ounces(radius,h):
+        ounces = h*(radius**2)
+        return ounces
+    
+    def recursive_helper(radius, h):
+        if radius == 0:
+            return 0
+        return get_cylinder_ounces(radius, h) + recursive_helper(radius - 1, h + 1)
+    
+    return recursive_helper(radius, 1) % 23412143"""
 
 #---to test your code, RUN this file---
 
